2dc1de99c3ac81813654f5d3edb3798175930169
Guilherme Siqueira Leite <ki_lua_741@hotmail.com>
2019-08-13 23:27:02 -0300

    Adicionar arquivos de projeto.

---
+++ TesteAgendamento/Agenda.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAgendamento
+{
+    public class Agenda
+    {
+       
+
+        public int Dia { get; private set; }
+        public int Ano { get; private set; }
+        public int Hora { get; private set; }
+        public int Mes { get; private set; }
+        public Agenda()
+        {
+            Dia = DateTime.Now.Day;
+            Ano = DateTime.Now.Year;
+            Hora = DateTime.Now.Hour;
+            Mes = DateTime.Now.Month;
+        }
+
+        public void getData()
+        {
+            Console.WriteLine($"Dia {Dia} do mes {Mes} de {Ano} horario {Hora}") ;
+
+        }
+
+        public void enviarData(int dia, int mes, int hora)
+        {
+            if (dia < Dia | hora < 10.00 | hora >23.00| mes < Mes)
+            {
+                Console.WriteLine("Por favor verifique a hora e data se está de acordo com nossa agenda!");
+            }
+            else
+            {
+                Console.WriteLine("Cadastrado com sucesso");
+            }
+            
+
+        }
+
+    }
+}
---
+++ TesteAgendamento/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace TesteAgendamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Agenda hora = new Agenda();
+
+            hora.getData();
+            hora.enviarData(14, 8, 22);
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
